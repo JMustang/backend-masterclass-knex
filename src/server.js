5cd7be71102497545f6f8fff7bc52ca8becd3ab9
JMustang <eecfredes@gmail.com>
2021-04-18 20:23:39 -0300

    feat:creando os controllers

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 // servidor nodejs => express
 const express = require("express");
+const routes = require("./routes");
+
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Holla!");
-});
+app.use(routes);
+
 app.listen(PORT, () => {
   console.log(`Rodando na porta => http://localhost:${PORT}`);
 });
