1afc31f20077cffb597eda5115eb7afb6ff8d6b5
JMustang <eecfredes@gmail.com>
2021-04-26 21:04:56 -0300

    feat: lidando com errors

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,4 +6,16 @@ module.exports = {
 
     return res.json(results);
   },
+
+  async create(req, res, next) {
+    try {
+      const { username } = req.body;
+      await knex("users").insert({
+        username,
+      });
+      return res.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
 };
--- src/server.js
+++ src/server.js
@@ -5,7 +5,14 @@ const routes = require("./routes");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use(routes);
+
+// catch all errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Rodando na porta => http://localhost:${PORT}`);
