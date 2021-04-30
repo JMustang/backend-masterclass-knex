7457061c26b5e569223d84fb78bd04b4d53d412b
JMustang <eecfredes@gmail.com>
2021-04-29 21:04:27 -0300

    feat:rota DELETE criada, CRUD completo

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,13 @@
 const knex = require("../database");
 
 module.exports = {
+  // rota GET
   async index(req, res) {
     const results = await knex("users");
 
     return res.json(results);
   },
-
+  // rota POST
   async create(req, res, next) {
     try {
       const { username } = req.body;
@@ -18,12 +19,24 @@ module.exports = {
       next(error);
     }
   },
+  // rota PUT
   async update(req, res, next) {
     try {
       const { username } = req.body;
       const { id } = req.params;
 
       await knex("users").update({ username }).where({ id });
+
+      return res.send();
+    } catch (error) {
+      next(error);
+    }
+  },
+  // rota DELETE
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+      await knex("users").where({ id }).delete();
 
       return res.send();
     } catch (error) {
--- src/server.js
+++ src/server.js
@@ -8,6 +8,13 @@ const PORT = 3000;
 app.use(express.json());
 app.use(routes);
 
+// notFound
+app.use((req, res, next) => {
+  const error = new Error("NOT FOUND!");
+  error.status = 404;
+  next(error);
+});
+
 // catch all errors
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
