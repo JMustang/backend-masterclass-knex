412c20bbd58d3d7d3efdfc8d0a486ef9f0deb972
JMustang <eecfredes@gmail.com>
2021-04-30 20:38:01 -0300

    feat:migration projects criada

---
+++ src/database/migrations/20210430232510_create_projects_table.js
@@ -0,0 +1,17 @@
+// Migrations
+exports.up = (knex) =>
+  knex.schema.createTable("projects", (table) => {
+    table.increments("id");
+    table.text("title");
+
+    // Relacinamentos
+    table
+      .integer("user_id")
+      .references("users.id")
+      .notNullable()
+      .onDelete("CASCADE");
+
+    table.timestamps(true, true);
+  });
+
+exports.down = (knex) => knex.schema.dropTable("projects");
