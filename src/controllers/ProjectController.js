faf4e9f2edc5fb6ac7968ecfce1f21e7fa84d46a
JMustang <eecfredes@gmail.com>
2021-05-04 21:41:55 -0300

    feat:paginacao de projetos criada

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -4,15 +4,24 @@ module.exports = {
   // rota GET
   async index(req, res, next) {
     try {
-      const { user_id } = req.query;
-      const query = knex("projects");
+      const { user_id, page = 1 } = req.query;
+      const query = knex("projects")
+        .limit(5)
+        .offset((page - 1) * 5);
+
+      const countObj = knex("projects").count();
 
       if (user_id) {
         query
           .where({ user_id })
           .join("users", "users.id", "=", "projects.user_id")
           .select("projects.*", "users.username");
+
+        countObj.where({ user_id });
       }
+
+      const [count] = await countObj;
+      res.header("X-Total-Count", count["count"]);
 
       const results = await query;
       return res.json(results);
